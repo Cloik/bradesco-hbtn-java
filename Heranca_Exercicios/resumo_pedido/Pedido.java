a548d6aa2095a12c4a4dc4e90dbc970fdcab46e0
Cloik <claudineialoik@gmail.com>
2025-07-20 17:45:08 -0300

    Task 7 - Resumo Pedido

---
+++ Heranca_Exercicios/resumo_pedido/Pedido.java
@@ -0,0 +1,65 @@
+import produtos.Livro;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Pedido {
+    private double percentualDesconto;
+    private ItemPedido[] itens;
+
+    public Pedido(double percentualDesconto, ItemPedido[] itens) {
+        this.percentualDesconto = percentualDesconto;
+        this.itens = itens;
+    }
+
+    public double getPercentualDesconto() {
+        return percentualDesconto;
+    }
+
+    public double calcularTotal(){
+        double total = 0.0;
+        for(ItemPedido item : itens){
+            total += item.getQuantidade() * item.getProduto().obterPrecoLiquido();
+        }
+        total -= total * (percentualDesconto / 100.0);
+        return total;
+    }
+
+    public void apresentarResumoPedido() {
+        Locale.setDefault(new Locale("pt", "BR"));
+        NumberFormat formato = NumberFormat.getNumberInstance(Locale.getDefault());
+        formato.setMinimumFractionDigits(2);
+        formato.setMaximumFractionDigits(2);
+
+        System.out.println("------- RESUMO PEDIDO -------");
+        double totalProdutos = 0.0;
+
+        for (ItemPedido item : itens) {
+            String tipo = item.getProduto() instanceof Livro ? "Livro" : "Dvd";
+            String titulo = item.getProduto().getTitulo();
+            double preco = item.getProduto().obterPrecoLiquido();
+            int quantidade = item.getQuantidade();
+            double totalItem = preco * quantidade;
+
+            totalProdutos += totalItem;
+
+            System.out.printf("Tipo: %s  Titulo: %s  Preco: %s  Quant: %d  Total: %s\n",
+                    tipo,
+                    titulo,
+                    formato.format(preco),
+                    quantidade,
+                    formato.format(totalItem));
+        }
+
+        System.out.println("----------------------------");
+
+        double valorDesconto = totalProdutos * (percentualDesconto / 100.0);
+        double totalPedido = totalProdutos - valorDesconto;
+
+        System.out.println("DESCONTO: " + formato.format(valorDesconto));
+        System.out.println("TOTAL PRODUTOS: " + formato.format(totalProdutos));
+        System.out.println("----------------------------");
+        System.out.println("TOTAL PEDIDO: " + formato.format(totalPedido));
+        System.out.println("----------------------------");
+    }
+}
