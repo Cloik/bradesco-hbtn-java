1a612089cbc4e160d2b7011b1d0c98db4e057b04
Cloik <claudineialoik@gmail.com>
2025-07-19 21:59:06 -0300

    Task 2

---
+++ Heranca/salario/Gerente.java
@@ -0,0 +1,20 @@
+package Heranca.salario;
+
+public class Gerente extends Empregado{
+
+    public Gerente(double salarioFixo) {
+        super(salarioFixo);
+    }
+
+    @Override
+    public double calcularBonus(Departamento departamento) {
+        if (departamento.alcancouMeta()) {
+            double bonusBase = getSalarioFixo() * 0.20;
+            double excedente = departamento.getValorAtingidoMeta() - departamento.getValorMeta();
+            double bonusExtra = excedente * 0.01;
+            return bonusBase + bonusExtra;
+        } else {
+            return 0.0;
+        }
+    }
+}
