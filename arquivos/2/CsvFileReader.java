8da3739c4a65bd0e44ec3ec05b74d309dafcfb60
Cloik <claudineialoik@gmail.com>
2025-08-16 00:06:57 -0300

    Task 2 - file handing - v1

---
+++ arquivos/2/CsvFileReader.java
@@ -0,0 +1,32 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CsvFileReader {
+    public static void main(String[] args) {
+        String arquivo = "funcionario.csv";
+        String linha;
+
+        try(BufferedReader buffer = new BufferedReader(new FileReader(arquivo))){
+//            buffer.readLine(); //Ignora o cabeçalho
+
+            while((linha = buffer.readLine()) != null){
+                if(linha.trim().isEmpty()){ //Ignora as linhas em branco
+                    continue;
+                }
+                String[] dados = linha.split(",");
+                if(dados.length == 4){
+                    System.out.println("Funcionário: " + dados[0]);
+                    System.out.println("Idade: " + dados[1]);
+                    System.out.println("Departamento: " + dados[2]);
+                    System.out.println("Salarial: " + dados[3]);
+                    System.out.println("------------------------");
+                }
+            }
+            System.out.println("Leitura do arquivo concluída.");
+        }catch (IOException e){
+            System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+        }
+    }
+}
